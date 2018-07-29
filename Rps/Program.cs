2e1a37f78b39fe32445450d1ab0788934c6d2558
shihhanwang <swang05@mail.bbk.ac.uk>
2018-07-29 18:36:57 +0100

    Initial check-in of module Rps

---
+++ Rps/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rps
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
