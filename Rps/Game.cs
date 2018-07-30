eb8491108fb1d8c1ddd6f490149219a2959aacd7
shihhanwang <swang05@mail.bbk.ac.uk>
2018-07-30 10:17:59 +0100

    test | Game | initialize with 2 players and 3 turns

---
+++ Rps/Game.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Rps
+{
+    public class Game
+    {
+        public Game()
+        {
+        }
+    }
+}
---
+++ Rps/UnitTests/GameTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Rps.Tests
+{
+    [TestFixture]
+    public class GameTest
+    {
+        Game game;
+
+        [SetUp]
+        public void GameSetUp() 
+        {
+            Player p1 = new Player("User");
+            Player p2 = new Player("Computer");
+
+            game = new Game(p1, p2);
+        }
+
+        [Test]
+        public void InitializeGameVariable()
+        {
+            Assert.That(game.TotalTurn, Is.EqualTo(3));
+            Assert.That(game.p1.Name, Is.EqualTo("User"));
+            Assert.That(game.p2.Name, Is.EqualTo("Computer"));
+        }
+
+    }
+}
