3126d587c5d6815c4fd88f2438983c4a780bdc0b
shihhanwang <swang05@mail.bbk.ac.uk>
2018-07-30 11:32:51 +0100

    Helper | Rps input validation & converting input tests and tests passed

--- Rps/Helper.cs
+++ Rps/Helper.cs
@@ -3,8 +3,38 @@ namespace Rps
 {
     public class Helper
     {
-        public Helper()
+        public static bool RpsValidation(char input)
         {
+            char inputUpper = Char.ToUpper(input);
+
+            if (inputUpper != 'R' && inputUpper != 'P' && inputUpper != 'S')
+            {
+                Console.WriteLine("Please enter the valid character");
+                return false;
+            }
+
+            return true;
+
+        }
+
+        public static Moves ConvertInput(char input)
+        {
+            Moves result = 0;
+
+            switch (Char.ToUpper(input))
+            {
+                case 'R':
+                    result = Moves.Rock;
+                    break;
+                case 'P':
+                    result = Moves.Paper;
+                    break;
+                case 'S':
+                    result = Moves.Scissors;
+                    break;
+            }
+
+            return result;
         }
     }
 }
---
+++ Rps/Tests/HelperTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Rps.Tests
+{
+    [TestFixture]
+    public class HelperTest
+    {
+
+        [Test]
+        [TestCase('R', true)]
+        [TestCase('P', true)]
+        [TestCase('S', true)]
+        [TestCase('A', false)]
+        [TestCase('W', false)]
+        public void RpsInputValidation(char input, bool expected)
+        {
+            bool output = Helper.RpsValidation(input);
+            Assert.That(output, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase('R', Moves.Rock)]
+        [TestCase('P', Moves.Paper)]
+        [TestCase('S', Moves.Scissors)]
+        public void RpsInputValidation(char input, Moves expected)
+        {
+            Moves output = Helper.ConvertInput(input);
+            Assert.That(output, Is.EqualTo(expected));
+        }
+
+    }
+}
