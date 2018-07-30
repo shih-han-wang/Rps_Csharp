cb3efdc114551c60418fafd261ac08d357a70dd9
shihhanwang <swang05@mail.bbk.ac.uk>
2018-07-30 18:05:54 +0100

    AIMove | tactical ai move tests and tests passed

--- Rps/AIMove.cs
+++ Rps/AIMove.cs
@@ -1,12 +1,50 @@
 ﻿using System;
 namespace Rps
 {
+    public enum Modes { Random = 1, Tactical };
+
     public class AIMove
     {
+
+        public Moves TacticalMove { get; set; }
+
+        public AIMove()
+        {
+            TacticalMove = this.RandChoice();
+        }
+
         public Moves RandChoice()
         {
             var rnd = new Random();
             return (Moves)rnd.Next(1, Enum.GetNames(typeof(Moves)).Length + 1);
+        }
+
+        public Modes UserChooseMode()
+        {
+            string userInput;
+            char inputChar;
+
+            do
+            {
+                Console.WriteLine(Message.ChooseMode());
+                userInput = Console.ReadLine();
+                inputChar = Convert.ToChar(userInput);
+
+            } while (!Helper.ChooseModeValidation(inputChar));
+
+            return Helper.ConvertModeInput(inputChar);
+
+        }
+
+        public Moves SetTacticalMove(Moves userInput)
+        {
+            int value = (int)userInput;
+
+            if (value == Enum.GetValues(typeof(Moves)).Length)
+                return (Moves)1;
+            else
+                return (Moves)value + 1;
+
         }
     }
 }
--- Rps/Tests/AIMoveTest.cs
+++ Rps/Tests/AIMoveTest.cs
@@ -6,12 +6,43 @@ namespace Rps.Tests
     [TestFixture]
     public class AIMoveTest
     {
+        AIMove aiMove;
+
+        [SetUp]
+        public void SetUp()
+        {
+            aiMove = new AIMove();
+        }
 
         [Test]
         public void ReturnRandChoice()
         {
-            AIMove aiMove = new AIMove();
             Assert.IsInstanceOf<Moves>(aiMove.RandChoice());
+        }
+
+        [Test]
+        public void TacticalMoveReturnRandByDefault()
+        {
+            Assert.IsInstanceOf<Moves>(aiMove.TacticalMove);
+        }
+
+        [Test]
+        [TestCase(Moves.Rock, Moves.Paper)]
+        [TestCase(Moves.Paper, Moves.Scissors)]
+        [TestCase(Moves.Scissors, Moves.Rock)]
+        public void ReturnNextTacticalMove(Moves input, Moves expected)
+        {
+            Moves output = aiMove.SetTacticalMove(input);
+
+            Assert.That(output, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void SetAndGetTacticalMove()
+        {
+            aiMove.TacticalMove = aiMove.SetTacticalMove(Moves.Rock);
+
+            Assert.That(aiMove.TacticalMove, Is.EqualTo(Moves.Paper));
         }
 
     }
