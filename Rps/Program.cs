2050718e0ae8177704b4172dc407b241955557a9
shihhanwang <swang05@mail.bbk.ac.uk>
2018-07-30 18:10:46 +0100

    Program | user can choose computer mode

--- Rps/Program.cs
+++ Rps/Program.cs
@@ -2,25 +2,38 @@
 
 namespace Rps
 {
-    class MainClass
+    class Program
     {
         public static void Main(string[] args)
         {
+
             Player p1 = new Player("User");
             Player p2 = new Player("Computer");
 
             Game game = new Game(p1, p2);
             AIMove aiMove = new AIMove();
 
+            Modes mode = aiMove.UserChooseMode();
+
             int totalTurn = game.TotalTurn;
             int halfTurn = (int)Math.Ceiling((double)totalTurn / 2);
 
             do
             {
-                game.p1.Choice = game.GetUerInput();
-                game.p2.Choice = aiMove.RandChoice();
+                game.p1.Choice = game.GetUserInput();
 
+                if (mode == Modes.Tactical)
+                {
+                    game.p2.Choice = aiMove.TacticalMove;
+                    aiMove.TacticalMove = aiMove.SetTacticalMove(game.p1.Choice);
+                }
+                else
+                {
+                    game.p2.Choice = aiMove.RandChoice();
+                }
+                    
                 game.TurnFinished();
+                Console.WriteLine("---------------------");
 
                 if (p1.score == halfTurn || p2.score == halfTurn)
                     break;
