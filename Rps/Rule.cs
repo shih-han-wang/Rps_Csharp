519758ce96b1d78bf60913a84cf7f9832283f67f
shihhanwang <swang05@mail.bbk.ac.uk>
2018-07-30 12:50:22 +0100

    Rule | return winner player test and test passed

---
+++ Rps/Rule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Rps
+{
+    public class Rule
+    {
+        public static Player Winner(Player p1, Player p2)
+        {
+            int p1Value = (int)p1.Choice;
+            int p2Value = (int)p2.Choice;
+
+            Player result = null;
+
+            if (p1Value % 3 + 1 == p2Value)
+            {
+                result = p2;
+                p2.score++;
+            }
+            else if (p2Value % 3 + 1 == p1Value)
+            {
+                result = p1;
+                p1.score++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Rps/Tests/RuleTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Rps.Tests
+{
+    [TestFixture]
+    public class RuleTest
+    {
+
+        Player p1;
+        Player p2;
+        Player p3;
+
+        [SetUp]
+        public void GameSetUp()
+        {
+            p1 = new Player("p1");
+            p2 = new Player("p2");
+            p3 = new Player("p3");
+
+            p1.Choice = Moves.Rock;
+            p2.Choice = Moves.Paper;
+            p3.Choice = Moves.Scissors;
+        }
+
+
+        [Test]
+        public void ReturnWinnerPlayer()
+        {
+            Player winner1 = Rule.Winner(p1, p2);
+            Player winner2 = Rule.Winner(p1, p3);
+            Player winner3 = Rule.Winner(p2, p3);
+            Player winner4 = Rule.Winner(p1, p1);
+
+            Assert.That(winner1.Name, Is.EqualTo(p2.Name));
+            Assert.That(winner2.Name, Is.EqualTo(p1.Name));
+            Assert.That(winner3.Name, Is.EqualTo(p3.Name));
+            Assert.That(winner4, Is.Null);
+        }
+
+    }
+}
